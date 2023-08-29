3fdd9c6700cf1eb098d88d8dd7fff0a263115ad2
Emanuele-colaci <emanuele.colaci@gmail.com>
2023-08-29 18:27:23 +0200

    fatto axios e stampato

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,7 +1,27 @@
 <script>
     import axios from 'axios';
     export default {
-        
+        data(){
+            return{
+                baseUrl: 'http://127.0.0.1:8000',
+                projects: [],
+                loading: true,
+            }
+        },
+        created(){
+            this.getProjects();
+        },
+        methods:{
+            getProjects(){
+                this.loading = true;
+                axios.get(`${this.baseUrl}/api/projects`).then((response) => {
+                    if(response.data.success){
+                        this.projects = response.data.results;
+                        this.loading = false;
+                    }
+                });
+            }
+        }
     }
 </script>
 <template>
@@ -12,9 +32,43 @@
                     <h1 class="text-center my-5">BOOLFOLIO</h1>
                 </div>
             </div>
+            <div class="row">
+                <div class="col-md-4" v-for="project in projects" :key="project.id">
+                    <div class="card min-height-200px mx-3">
+                        <div class="card-image-container">
+                            <img :src="`${this.baseUrl}/storage/${project.image}`" class="card-img-top my-img">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">{{ project.titolo }}</h5>
+                            <p class="card-text">{{ project.descrizione }}</p>
+                            <p class="card-text"><small class="text-muted">{{ project.data }}</small></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 <style lang="scss">
     @use '../styles/generals.scss' as *;
+
+
+.min-height-200px{
+    min-height: 200px;
+}
+.card-image-container{
+    position: relative;
+    overflow: hidden;
+    padding-top: 56.25%; /* 16:9 aspect ratio (adjust as needed) */
+    
+    .my-img{
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+}
+
 </style>
