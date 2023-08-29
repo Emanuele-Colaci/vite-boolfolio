3b058929175044df8cf6695da0579d21e9088066
Emanuele-colaci <emanuele.colaci@gmail.com>
2023-08-29 19:46:44 +0200

    esercizio fatto

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -10,18 +10,28 @@
                 baseUrl: 'http://127.0.0.1:8000',
                 projects: [],
                 loading: true,
-                maxCaracters: 80
+                maxCaracters: 70,
+                currentPage: 1,
+                lastPage: null
             }
         },
         created(){
-            this.getProjects();
+            this.getProjects(1);
         },
         methods:{
-            getProjects(){
+            getProjects(num_page){
                 this.loading = true;
-                axios.get(`${this.baseUrl}/api/projects`).then((response) => {
+                // axios.get(`${this.baseUrl}/api/projects`).then((response) => {
+                //     if(response.data.success){
+                //         this.projects = response.data.results;
+                //         this.loading = false;
+                //     }
+                // });
+                axios.get(`${this.baseUrl}/api/projects`, {params: { page: num_page }}).then((response) => {
                     if(response.data.success){
-                        this.projects = response.data.results;
+                        this.projects = response.data.results.data;
+                        this.currentPage = response.data.results.current_page;
+                        this.lastPage = response.data.results.last_page;
                         this.loading = false;
                     }
                 });
@@ -36,6 +46,7 @@
 </script>
 <template>
     <div>
+        <AppLoading v-if="loading"/>
         <div class="container">
             <div class="row">
                 <div class="col-12">
@@ -43,9 +54,8 @@
                 </div>
             </div>
             <div class="row">
-                <AppLoading />
                 <div class="col-12 col-md-4" v-for="project in projects" :key="project.id">
-                    <div class="card min-height-380px my-3">
+                    <div class="card min-height-500px my-3">
                         <div class="card-image-container">
                             <img :src="`${this.baseUrl}/storage/${project.image}`" class="card-img-top my-img" v-if="project.image">
                             <img src="https://picsum.photos/200/300" class="card-img-top my-img" v-else>
@@ -54,7 +64,38 @@
                             <h5 class="card-title">{{ project.titolo }}</h5>
                             <p class="card-text">{{ turncateText(project.descrizione) }}</p>
                             <p class="card-text"><small class="text-muted">{{ project.data }}</small></p>
+                            
+                            <!-- Visualizza il tipo -->
+                            <p class="card-text"><strong>Type:</strong> {{ project.type ? project.type.name : 'N/A' }}</p>
+                            
+                            <!-- Visualizza le tecnologie -->
+                            <p class="card-text">
+                                <strong>Tecnologie:</strong>
+                                <span v-for="(technology, index) in project.technologies" :key="index">
+                                    {{ technology.name }}{{ index < project.technologies.length - 1 ? ', ' : '' }}
+                                </span>
+                                <span v-if="project.technologies.length === 0">N/A</span>
+                            </p>
                         </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <div class="d-flex justify-content-center">
+                        <nav>
+                            <ul class="pagination">
+                                <li :class="currentPage === 1 ? 'disabled' : ''">
+                                    <button class="page-link" @click="getProjects(currentPage - 1)">Precedente</button>
+                                </li>
+                                <li :class="currentPage === lastPage ? 'disabled' : ''">
+                                    <button class="page-link" @click="getProjects(currentPage + 1)">Successivo</button>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div class="text-center mt-3">
+                        Pagina {{ currentPage }} di {{ lastPage }}
                     </div>
                 </div>
             </div>
@@ -65,13 +106,14 @@
     @use '../styles/generals.scss' as *;
 
 
-.min-height-380px{
-    min-height: 380px;
+.min-height-500px{
+    min-height: 500px;
 }
+
 .card-image-container{
     position: relative;
     overflow: hidden;
-    padding-top: 56.25%; /* 16:9 aspect ratio (adjust as needed) */
+    padding-top: 56.25%;
     
     .my-img{
         position: absolute;
