227cb7f23fcca7e6fc45d89b297b259321e05745
Emanuele-colaci <emanuele.colaci@gmail.com>
2023-08-29 16:26:56 +0200

    AppMain create and import in app.vue

---
+++ src/components/AppMain.vue
@@ -0,0 +1,20 @@
+<script>
+    import axios from 'axios';
+    export default {
+        
+    }
+</script>
+<template>
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1 class="text-center my-5">BOOLFOLIO</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="scss">
+    @use '../styles/generals.scss' as *;
+</style>
